602bc924d0c4a160833a2a20c0b446f33143e6ae
discord <julian.desvignes@telecom-sudparis.eu>
2016-01-07 12:37:17 +0100

    doc

--- src/enums/Bandwidth.java
+++ src/enums/Bandwidth.java
@@ -5,10 +5,10 @@ package enums;
  */
 public enum Bandwidth
 {
-    NULL(0),
-    ETH_100(100),
-    SERIAL(3),
-    ETH_1G(1000);
+    NULL(0),         // Placeholder pour la création d'un lien
+    ETH_100(100),    // Fast Ethernet 100Mbps
+    SERIAL(3),       // Série 64kbps
+    ETH_1G(1000);    // Gigabit Ethernet 1Gbps
 
 
     public int value;
